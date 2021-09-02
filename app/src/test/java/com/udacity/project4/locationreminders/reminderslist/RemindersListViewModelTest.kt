2dae1123ea04c8b7d64e280ec6ed1e781d054665
lsalgado <lsalgado@muxi.com.br>
2021-09-02 20:23:56 -0300

    Adding first viewmodel test.

--- app/src/test/java/com/udacity/project4/locationreminders/reminderslist/RemindersListViewModelTest.kt
+++ app/src/test/java/com/udacity/project4/locationreminders/reminderslist/RemindersListViewModelTest.kt
@@ -1,7 +1,17 @@
 package com.udacity.project4.locationreminders.reminderslist
 
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.udacity.project4.locationreminders.MainCoroutineRule
+import com.udacity.project4.locationreminders.data.FakeDataSource
+import com.udacity.project4.locationreminders.data.dto.ReminderDTO
+import getOrAwaitValue
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.hamcrest.CoreMatchers
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
@@ -9,5 +19,40 @@ import org.junit.runner.RunWith
 class RemindersListViewModelTest {
 
     //TODO: provide testing to the RemindersListViewModel and its live data objects
+    @get:Rule
+    val mainCoroutineRule = MainCoroutineRule()
+
+    private lateinit var viewModel: RemindersListViewModel
+    private lateinit var dataSource: FakeDataSource
+
+    @Before
+    fun setUp() {
+        // Given a fresh ViewModel
+        //  tasksViewModel = TasksViewModel(ApplicationProvider.getApplicationContext())
+        dataSource = FakeDataSource()
+        val reminder1 = ReminderDTO("Title1", "Description1", "Location1", 100.0, 100.0)
+        val reminder2 = ReminderDTO("Title2", "Description2", "Location2", 200.0, 200.0)
+        val reminder3 = ReminderDTO("Title3", "Description3", "Location3", 300.0, 300.0)
+        dataSource.saveReminders(reminder1, reminder2, reminder3)
+
+        viewModel = RemindersListViewModel(ApplicationProvider.getApplicationContext(), dataSource)
+    }
+
+    @Test
+    fun loadReminders_loading() {
+        mainCoroutineRule.pauseDispatcher()
+        viewModel.loadReminders()
+
+        Assert.assertThat(
+            viewModel.showLoading.getOrAwaitValue(),
+            CoreMatchers.`is`(true)
+        )
+
+        mainCoroutineRule.resumeDispatcher()
+        Assert.assertThat(
+            viewModel.showLoading.getOrAwaitValue(),
+            CoreMatchers.`is`(false)
+        )
+    }
 
 }
