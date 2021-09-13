7288ed45a5acec053963745ef678a3592a36ddc8
lsalgado <lsalgado@muxi.com.br>
2021-09-13 16:18:56 -0300

    Adding RemindersActivity tests.

--- app/src/androidTest/java/com/udacity/project4/RemindersActivityTest.kt
+++ app/src/androidTest/java/com/udacity/project4/RemindersActivityTest.kt
@@ -1,16 +1,30 @@
 package com.udacity.project4
 
 import android.app.Application
+import androidx.room.Room
+import androidx.test.core.app.ActivityScenario
 import androidx.test.core.app.ApplicationProvider.getApplicationContext
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.assertion.ViewAssertions.doesNotExist
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
+import com.udacity.project4.locationreminders.RemindersActivity
 import com.udacity.project4.locationreminders.data.ReminderDataSource
 import com.udacity.project4.locationreminders.data.local.LocalDB
+import com.udacity.project4.locationreminders.data.local.RemindersDatabase
 import com.udacity.project4.locationreminders.data.local.RemindersLocalRepository
 import com.udacity.project4.locationreminders.reminderslist.RemindersListViewModel
 import com.udacity.project4.locationreminders.savereminder.SaveReminderViewModel
+import com.udacity.project4.locationreminders.savereminder.selectreminderlocation.SelectLocationFragment
+import com.udacity.project4.util.DataBindingIdlingResource
+import com.udacity.project4.util.monitorActivity
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.runBlocking
 import org.junit.Before
+import org.junit.Test
 import org.junit.runner.RunWith
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.core.context.startKoin
@@ -27,6 +41,7 @@ class RemindersActivityTest :
 
     private lateinit var repository: ReminderDataSource
     private lateinit var appContext: Application
+    val dataBindingIdlingResource = DataBindingIdlingResource()
 
     /**
      * As we use Koin as a Service Locator Library to develop our code, we'll also use Koin to test our code.
@@ -49,7 +64,12 @@ class RemindersActivityTest :
                     get() as ReminderDataSource
                 )
             }
-            single { RemindersLocalRepository(get()) as ReminderDataSource }
+            // Using inMemoryDatabaseBuilder to avoid overwriting the device's data
+            val database =
+                Room.inMemoryDatabaseBuilder(getApplicationContext(), RemindersDatabase::class.java)
+                    .allowMainThreadQueries().build()
+            val repository = RemindersLocalRepository(database.reminderDao(), Dispatchers.Main)
+            single { repository as ReminderDataSource }
             single { LocalDB.createRemindersDao(appContext) }
         }
         //declare a new koin module
@@ -65,7 +85,109 @@ class RemindersActivityTest :
         }
     }
 
+    @Test
+    fun createReminder_saveReminder_listReminder() {
+        val activityScenario = ActivityScenario.launch(RemindersActivity::class.java)
+        dataBindingIdlingResource.monitorActivity(activityScenario)
 
-//    TODO: add End to End testing to the app
+        val title = "Title"
+        val desc = "Description"
+        val location = "Location"
 
+        onView(withId(R.id.addReminderFAB)).perform(click())
+        onView(withId(R.id.reminderTitle)).perform(replaceText(title))
+        onView(withId(R.id.reminderDescription)).perform(replaceText(desc))
+
+        onView(withId(R.id.selectLocation)).perform(click())
+
+        onView(withText(R.string.please_select_location)).check(matches(isDisplayed()))
+        onView(withId(R.id.mapFragment)).check(matches(isDisplayed()))
+
+        onView(withId(R.id.mapFragment)).perform(longClick())
+
+        onView(withText(R.string.location_name)).check(matches(isDisplayed()))
+        onView(withId(SelectLocationFragment.dialogEditTextId!!)).perform(replaceText(location))
+        onView(withText("OK")).perform(click())
+
+        onView(withText(R.string.save)).check(matches(isDisplayed())).perform(click())
+
+        onView(withText(title)).check(matches(isDisplayed()))
+        onView(withText(location)).check(matches(isDisplayed()))
+
+        onView(withId(R.id.saveReminder)).perform(click())
+
+        onView(withId(R.id.addReminderFAB)).check(matches(isDisplayed()))
+        onView(withText(title)).check(matches(isDisplayed()))
+        onView(withText(location)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun createReminder_missingTitle_showSnackbar() {
+        val activityScenario = ActivityScenario.launch(RemindersActivity::class.java)
+        dataBindingIdlingResource.monitorActivity(activityScenario)
+
+        val desc = "Description"
+        onView(withId(R.id.addReminderFAB)).perform(click())
+        onView(withId(R.id.reminderDescription)).perform(replaceText(desc))
+
+        onView(withId(R.id.saveReminder)).perform(click())
+
+        onView(withText(R.string.err_enter_title))
+    }
+
+    @Test
+    fun createReminder_missingLatLng_showSnackbar() {
+        val activityScenario = ActivityScenario.launch(RemindersActivity::class.java)
+        dataBindingIdlingResource.monitorActivity(activityScenario)
+
+        val title = "Title"
+        val desc = "Description"
+        onView(withId(R.id.addReminderFAB)).perform(click())
+        onView(withId(R.id.reminderTitle)).perform(replaceText(title))
+        onView(withId(R.id.reminderDescription)).perform(replaceText(desc))
+
+        onView(withId(R.id.saveReminder)).perform(click())
+
+        onView(withText(R.string.err_select_location))
+    }
+
+    @Test
+    fun createReminder_cancel_showNoData() {
+        val activityScenario = ActivityScenario.launch(RemindersActivity::class.java)
+        dataBindingIdlingResource.monitorActivity(activityScenario)
+
+        val title = "Title"
+        val desc = "Description"
+        val location = "Location"
+
+        onView(withId(R.id.addReminderFAB)).perform(click())
+        onView(withId(R.id.reminderTitle)).perform(replaceText(title))
+        onView(withId(R.id.reminderDescription)).perform(replaceText(desc))
+
+        onView(withId(R.id.selectLocation)).perform(click())
+
+        onView(withText(R.string.please_select_location)).check(matches(isDisplayed()))
+        onView(withId(R.id.mapFragment)).check(matches(isDisplayed()))
+
+        onView(withId(R.id.mapFragment)).perform(longClick())
+
+        onView(withText(R.string.location_name)).check(matches(isDisplayed()))
+        onView(withId(SelectLocationFragment.dialogEditTextId!!)).perform(replaceText(location))
+        onView(withText("OK")).perform(click())
+
+        activityScenario.onActivity { it.onBackPressed() }
+
+        onView(withText(title)).check(matches(isDisplayed()))
+        onView(withText(location)).check(matches(isDisplayed()))
+
+        onView(withId(R.id.saveReminder)).check(matches(isDisplayed()))
+
+        activityScenario.onActivity { it.onBackPressed() }
+
+        onView(withId(R.id.addReminderFAB)).check(matches(isDisplayed()))
+
+        onView(withText(title)).check(doesNotExist())
+        onView(withText(location)).check(doesNotExist())
+        onView(withId(R.id.noDataTextView)).check(matches(isDisplayed()))
+    }
 }
