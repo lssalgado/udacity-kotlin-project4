a49aefa806b65822c644d4cc32375cea0ca90091
lsalgado <lsalgado@muxi.com.br>
2021-09-13 12:36:02 -0300

    Adding RemindersLocalRepository tests.

--- app/src/androidTest/java/com/udacity/project4/locationreminders/data/local/RemindersLocalRepositoryTest.kt
+++ app/src/androidTest/java/com/udacity/project4/locationreminders/data/local/RemindersLocalRepositoryTest.kt
@@ -7,17 +7,17 @@ import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.MediumTest
 import com.udacity.project4.locationreminders.data.dto.ReminderDTO
 import com.udacity.project4.locationreminders.data.dto.Result
+import com.udacity.project4.util.compareReminders
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.runBlockingTest
 import org.hamcrest.CoreMatchers.`is`
 import org.hamcrest.CoreMatchers.instanceOf
 import org.hamcrest.MatcherAssert.assertThat
-import org.junit.After
-import org.junit.Before
-import org.junit.Rule
-import org.junit.Test
+import org.junit.*
 import org.junit.runner.RunWith
+import org.koin.core.context.stopKoin
 
 @ExperimentalCoroutinesApi
 @RunWith(AndroidJUnit4::class)
@@ -25,6 +25,90 @@ import org.junit.runner.RunWith
 @MediumTest
 class RemindersLocalRepositoryTest {
 
-//    TODO: Add testing implementation to the RemindersLocalRepository.kt
+    @get:Rule
+    val instantTaskExecutorRule = InstantTaskExecutorRule()
 
+    private lateinit var database: RemindersDatabase
+    private lateinit var remindersLocalRepository: RemindersLocalRepository
+
+    @Before
+    fun setUp() {
+        database = Room.inMemoryDatabaseBuilder(
+            ApplicationProvider.getApplicationContext(),
+            RemindersDatabase::class.java
+        ).allowMainThreadQueries().build()
+
+        remindersLocalRepository = RemindersLocalRepository(database.reminderDao(), Dispatchers.Main)
+    }
+
+    @After
+    fun tearDown() {
+        database.close()
+        stopKoin()
+    }
+
+    @Test
+    fun saveReminderAndGetReminder() = runBlocking {
+        val reminder = ReminderDTO("Title", "Description", "Location", 0.0, 100.0)
+        remindersLocalRepository.saveReminder(reminder)
+
+        val result = remindersLocalRepository.getReminder(reminder.id)
+
+        assertThat(result, instanceOf(Result.Success::class.java))
+        result as Result.Success
+        compareReminders(reminder, result.data)
+    }
+
+    @Test
+    fun saveMultipleRemindersAndGetEachByID() = runBlocking {
+        val reminders = arrayOf(
+            ReminderDTO("Title", "Description", "Location", 1.0, 100.0),
+            ReminderDTO("Title2", "Description2", "Location2", 2.0, 200.0),
+            ReminderDTO("Title3", "Description3", "Location3", 3.0, 300.0),
+            ReminderDTO("Title4", "Description4", "Location4", 4.0, 400.0)
+        )
+
+        reminders.forEach { reminder ->
+            remindersLocalRepository.saveReminder(reminder)
+        }
+
+        reminders.forEach { reminder ->
+            val result = remindersLocalRepository.getReminder(reminder.id)
+
+            assertThat(result, instanceOf(Result.Success::class.java))
+            result as Result.Success
+            compareReminders(reminder, result.data)
+        }
+    }
+
+    @Test
+    fun saveMultipleRemindersAndGetAll() = runBlocking {
+        val reminders = arrayOf(
+            ReminderDTO("Title", "Description", "Location", 1.0, 100.0),
+            ReminderDTO("Title2", "Description2", "Location2", 2.0, 200.0),
+            ReminderDTO("Title3", "Description3", "Location3", 3.0, 300.0),
+            ReminderDTO("Title4", "Description4", "Location4", 4.0, 400.0)
+        )
+
+        reminders.forEach { reminder ->
+            remindersLocalRepository.saveReminder(reminder)
+        }
+
+        val result = remindersLocalRepository.getReminders()
+
+        assertThat(result, instanceOf(Result.Success::class.java))
+        result as Result.Success
+        val dbReminders = result.data
+        assertThat(dbReminders.size, `is`(reminders.size))
+        reminders.forEach { reminder ->
+            try {
+                val dbReminder = dbReminders.single { it.id == reminder.id }
+                compareReminders(reminder, dbReminder)
+            } catch (e: IllegalArgumentException) {
+                Assert.fail("More than one reminder was found with the given ID!!")
+            } catch (e: NoSuchElementException) {
+                Assert.fail("No reminder was found with the given ID!!")
+            }
+        }
+    }
 }
